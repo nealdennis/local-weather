3dbbb0bd24cfe57275e9f4631f3144ff500dfc9c
nealdennis <neal.dennis@mailfence.com>
2019-07-29 04:46:03 +1000

    building out hourly temp fetching

--- weather.js
+++ weather.js
@@ -5,7 +5,22 @@ window.addEventListener("load", () => {
   getLocation();
 });
 
-function getLocation() {
+const convertTime = ( time ) => {
+  let theDate = new Date(time * 1000);
+  let hour = theDate.getHours();
+  let mins = "0" + theDate.getMinutes();
+  return(`${ hour }:${ mins }`);
+}
+
+const convertToC = ( temp ) => {
+  return Math.round((temp - 32) / 1.8);
+}
+
+const convertToF = ( temp ) => {
+  return Math.round((temp * 1.8) + 32);
+}
+
+const getLocation = () => {
   const location = "https://ipapi.co/json/";
 
   fetch(location)
@@ -22,7 +37,34 @@ function getLocation() {
     });
 }
 
-function getWeather(lat, long) {
+const displayHourlyTemp = (hourly) => {
+  hourly.data.forEach(hour => {
+    let time = convertTime(hour.time);
+    console.log(`${ time } - ${ convertToC(hour.temperature) }`);
+  });
+}
+
+const setIcons = (icon, iconID) => {
+  const skycons = new Skycons({ color: "white" });
+  const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+  skycons.play();
+  return skycons.set(iconID, Skycons[currentIcon]);
+}
+
+const changeDegree = () => {
+  const temp = document.querySelector(".temperature-degree");
+  const tempSpan = document.querySelector(".degree-container span");
+
+  if (tempSpan.textContent === "C") {
+    temp.textContent = convertToF(temp.textContent);
+    tempSpan.textContent = "F";
+  } else {
+    temp.textContent = convertToC(temp.textContent);
+    tempSpan.textContent = "C";
+  }
+}
+
+const getWeather = (lat, long) => {
   const timezone = document.querySelector(".location-local");
   const temp = document.querySelector(".temperature-degree");
   const tempDescription = document.querySelector(".temperature-description");
@@ -39,33 +81,18 @@ function getWeather(lat, long) {
     return response.json();
   })
   .then(data => {
-    const {temperature, summary, icon} = data.currently; 
+    const { temperature, icon } = data.currently; 
+    const { summary } = data.hourly;
     const city = data.timezone.split("/");
 
-    temp.textContent = Math.round((temperature - 32) / 1.8);
+    temp.textContent = convertToC(temperature);
     tempDescription.textContent = summary;
     timezone.textContent = `${city[1]}, ${city[0]}`;
     //Set icon
     setIcons(icon, document.querySelector(".icon"));
+
+    //Show next 24hrs
+    displayHourlyTemp(data.hourly)
   });
 }
 
-function setIcons(icon, iconID) {
-  const skycons = new Skycons({ color: "white" });
-  const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-  skycons.play();
-  return skycons.set(iconID, Skycons[currentIcon]);
-}
-
-function changeDegree() {
-  const temp = document.querySelector(".temperature-degree");
-  const tempSpan = document.querySelector(".degree-container span");
-
-  if (tempSpan.textContent === "C") {
-    temp.textContent = Math.round((temp.textContent * 1.8) + 32);
-    tempSpan.textContent = "F";
-  } else {
-    temp.textContent = Math.round((temp.textContent - 32) / 1.8);
-    tempSpan.textContent = "C";
-  }
-}
